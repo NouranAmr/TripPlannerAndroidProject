ea84bf40b7b50d933dacbb9d0ebff5c95a5444c6
AhmedAshrafFayeq <47187014+AhmedAshrafFayeq@users.noreply.github.com>
2019-04-21 18:09:12 +0200

    alarmcomit

--- TripPlannerProject/app/src/main/java/iti/jets/mad/tripplannerproject/screens/addtripscreen/alarmpackage/AlarmActivity.java
+++ TripPlannerProject/app/src/main/java/iti/jets/mad/tripplannerproject/screens/addtripscreen/alarmpackage/AlarmActivity.java
@@ -1,28 +1,34 @@
 package iti.jets.mad.tripplannerproject.screens.addtripscreen.alarmpackage;
 
+import android.app.AlarmManager;
 import android.app.AlertDialog;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.media.RingtoneManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 import iti.jets.mad.tripplannerproject.R;
+import iti.jets.mad.tripplannerproject.screens.addtripscreen.alarmbroadcast.AlertReciever;
 
 
 public class AlarmActivity extends AppCompatActivity {
 
     private AlertDialog.Builder alertBuilder;
     private MediaPlayer player;
-
+    AlarmPresenter presenter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
-        setFinishOnTouchOutside(false);
 
-        Log.i("ddd","ddd");
+        presenter=new AlarmPresenter(this);
+        setFinishOnTouchOutside(false);
 
         player=MediaPlayer.create(this, RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM));
         player.setLooping(true);
@@ -38,7 +44,8 @@ public class AlarmActivity extends AppCompatActivity {
                         player.stop();
                         player.release();
 
-                        //StartTrip
+                        //Start Trip   ---open google map
+                        presenter.startTrip();
                         finish();
                     }
                 }).setNeutralButton("snooze", new DialogInterface.OnClickListener() {
@@ -48,6 +55,7 @@ public class AlarmActivity extends AppCompatActivity {
                 player.release();
 
                 //Presenter   snooze trip
+                presenter.snoozeTrip();
             }
         }).setNegativeButton("cancel", new DialogInterface.OnClickListener() {
             @Override
@@ -55,10 +63,20 @@ public class AlarmActivity extends AppCompatActivity {
                 player.stop();
                 player.release();
 
+                cancelAlarm();
                 //presenter  cancel trip
+
                 finish();
             }
         }).setIcon(getResources().getDrawable(R.drawable.ic_notifications_active_black_24dp)).setCancelable(false).show();
 
+    }
+    private void cancelAlarm(){
+        AlarmManager alarmManager =(AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        Intent intent=new Intent(this, AlertReciever.class);
+        PendingIntent pendingIntent=PendingIntent.getBroadcast(this,1,intent,0);
+
+        alarmManager.cancel(pendingIntent);
+        Toast.makeText(AlarmActivity.this, "Alarm Canceled", Toast.LENGTH_SHORT).show();
     }
 }
